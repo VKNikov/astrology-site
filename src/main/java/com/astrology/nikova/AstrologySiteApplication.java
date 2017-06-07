16c5fcf970567a22c70c88e75ddaf172563bad37
Vassil Nikov <vassiln@yahoo.com>
2017-06-07 23:44:50 +0300

    Initial commit.

---
+++ src/main/java/com/astrology/nikova/AstrologySiteApplication.java
@@ -0,0 +1,12 @@
+package com.astrology.nikova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrologySiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrologySiteApplication.class, args);
+	}
+}
